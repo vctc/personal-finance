3000176b9d184ccb2e78b6b217f5bb7da4f5b061
Your Name <victorrajkumar1@gmail.com>
2023-08-28 23:13:57 +0530

    initial setup: expo eslint prettier husky

--- App.tsx
+++ App.tsx
@@ -1,15 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,3 +9,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const App = () => (
+  <View style={styles.container}>
+    <Text>Meow up App.js to start working on your app!</Text>
+    <StatusBar />
+  </View>
+);
+
+export default App;
---
+++ metro.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+config.resolver.sourceExts.push("cjs");
+
+module.exports = config;
